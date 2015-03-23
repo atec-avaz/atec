a1870f4c01db7471cc92297c98231d981a943de8
antonio.vaz <antonio.vaz.000650@edu.atec.pt>
2015-03-23 16:12:44 +0200

    Ordenar Lista Ligada

--- PCE/PCE/Main.cpp
+++ PCE/PCE/Main.cpp
@@ -7,40 +7,88 @@ struct node{
 	node* next;
 };
 
+void inserirNoInicio(node * & raiz, int numero);
 void inserirNoFinal(node * &raiz, int numero);
-const int MAX_SIZE = 10;
-void calcularValores2(int arr[], int *soma, float *media);
-void calcularValores(int arr[], int &soma, float &media);
-void troca1(int &valor1, int &valor2);
-void troca2(int *valor1, int *valor2);
+void inserirDepoisDe(node * & raiz, int numero, int pesquisa);
+void listarLista(node* ap);
+void ordenarListaAsc(node * & raiz);
+void ordenarListaDesc(node * & raiz);
 
 int main()
-{
-	
-	// Cabeça da lista
+{	
+	// raiz
 	node * root = NULL;
+	int i = 1;
+	inserirNoFinal(root, i++);
+	inserirNoFinal(root, i++);
+	inserirNoFinal(root, i++);
+	inserirNoFinal(root, i++);
+	inserirNoFinal(root, i++);
+	inserirNoFinal(root, i++);
+	inserirNoFinal(root, i++);
+	inserirNoFinal(root, i++);
+	inserirNoFinal(root, i++);
+	inserirNoFinal(root, 10);
 
-	int i = 0;
-	inserirNoFinal(root, i++);
-	inserirNoFinal(root, i++);
-	inserirNoFinal(root, i++);
-	inserirNoFinal(root, i++);
-	inserirNoFinal(root, i++);
+	inserirNoInicio(root, 50);
+	inserirDepoisDe(root, 7850, 50);
 
-	
-	node* iter = root;
-	int posicao = 0;
-	do {
-		cout << "Posicao " << posicao++ << " => " << iter->valor << endl;
-		iter = iter->next;
-	} while (iter != NULL);
+	listarLista(root);
 
+	ordenarListaAsc(root);	
+	listarLista(root);
+
+	ordenarListaDesc(root);
+	listarLista(root);
+		
 	system("pause");
 }
 
-void inserirNoFinal(node * &raiz, int numero){
+void listarLista(node* ap){
+	int posicao = 0;
+	do {
+		cout << "Posicao " << posicao++ << " => " << ap->valor << endl;
+		ap = ap->next;
+	} while (ap != NULL);
+}
+
+void inserirDepoisDe(node * & raiz, int numero, int pesquisa){
 	node * novo;
+	novo = new node;
+	novo->valor = numero;
 	
+	// ligações
+	if (raiz == NULL){
+		// lista vazia
+		novo->next = NULL;
+		raiz = novo;
+	}
+	else{
+		// Lista com elemento(s)
+		node* iter = raiz;
+		while (iter->next != NULL){
+			if (iter->valor == pesquisa)
+				break;
+
+			iter = iter->next;
+		}
+
+		novo->next = iter->next;
+		iter->next = novo;
+	}
+
+}
+
+void inserirNoInicio(node * & raiz, int numero){
+	node * novo;
+	novo = new node;
+	novo->valor = numero;
+	novo->next = raiz;
+	raiz = novo;
+}
+
+void inserirNoFinal(node * & raiz, int numero){
+	node * novo;
 	novo = new node;
 	novo->valor = numero;
 	novo->next = NULL;
@@ -49,40 +97,106 @@ void inserirNoFinal(node * &raiz, int numero){
 	last = raiz;
 	if (last == NULL){
 		raiz = novo;
-	} else{
-		while (last->next!=NULL){
+	}
+	else{
+		while (last->next != NULL){
 			last = last->next;
 		}
 		if (last != NULL)
 			last->next = novo;
-	}	
-}
-
-void calcularValores2(int arr[], int *soma, float *media){
-	for (int i = 0; i < MAX_SIZE; i++){
-		*soma += arr[i];
 	}
-	*media = (float) *soma / (float)MAX_SIZE;
 }
 
+void ordenarListaAsc(node * & raiz)
+{
+	if (raiz != 0)
+	{
+		node* current = raiz;
+		node* prev = 0;
+		node* tempNode = 0;
+		bool hasChanges = false;
 
-void calcularValores(int arr[], int &soma, float &media){
-	for (int i = 0; i < MAX_SIZE; i++){
-		soma += arr[i];
+		while (!hasChanges)
+		{
+			while (current->next != 0)
+			{
+				tempNode = current->next;
+
+				if (current->valor > tempNode->valor)
+				{
+					hasChanges = true;
+					current->next = tempNode->next;
+					tempNode->next = current;
+					
+					if (prev != NULL)
+						prev->next = tempNode;
+
+					prev = tempNode;
+
+					if (raiz == current)
+						raiz = tempNode;					
+				}
+				else
+				{
+					prev = current;
+					current = current->next;
+				}
+			}
+			if (hasChanges == false)
+				break;
+			else
+			{
+				prev = NULL;
+				current = raiz;
+				hasChanges = false;
+			}
+		}
 	}
-	media = (float) soma / (float) MAX_SIZE;
 }
 
-void troca1(int &valor1, int &valor2){
-	int tmp;
-	tmp = valor1;
-	valor1 = valor2;
-	valor2 = tmp;
-}
 
-void troca2(int *valor1, int *valor2){
-	int tmp;
-	tmp = *valor1;
-	*valor1 = *valor2;
-	*valor2 = tmp;
+void ordenarListaDesc(node * & raiz)
+{
+	if (raiz != 0)
+	{
+		node* current = raiz;
+		node* prev = 0;
+		node* tempNode = 0;
+		bool hasChanges = false;
+
+		while (!hasChanges)
+		{
+			while (current->next != 0)
+			{
+				tempNode = current->next;
+
+				if (current->valor < tempNode->valor)
+				{
+					hasChanges = true;
+					current->next = tempNode->next;
+					tempNode->next = current;
+
+					if (prev != NULL)
+						prev->next = tempNode;
+
+					prev = tempNode;
+					if (raiz == current)
+						raiz = tempNode;
+				}
+				else
+				{
+					prev = current;
+					current = current->next;
+				}
+			}
+			if (hasChanges == false)
+				break;
+			else
+			{
+				prev = NULL;
+				current = raiz;
+				hasChanges = false;
+			}
+		}
+	}
 }
